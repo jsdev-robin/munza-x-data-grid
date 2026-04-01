3d7e5afa6152a164d6ec0ec9bdbd8ea30b5e6854
jsdev-robin <jsdev.robin@gmail.com>
2026-04-01 23:42:27 +0600

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,40 @@
 import React from 'react';
-import { Checkbox } from './components/ui/checkbox';
 import { useStores, type TStore } from './hooks/useStores';
 import { Grid, useGridState, type ColumnDef } from './index';
 
 const App = () => {
   const { data, isLoading, isError, isFetching } = useStores();
 
-  console.log(data);
-
   const columns = React.useMemo<ColumnDef<TStore, unknown>[]>(
     () => [
       {
-        accessorFn: (_row, index) => index + 1,
-        cell: ({ row }) => row.index + 1,
-        id: 'rowNumber',
-        header: '',
-        size: 54,
-        maxSize: 54,
-        enableColumnFilter: false,
-      },
-      {
-        id: 'select',
-        header: ({ table }) => (
-          <Checkbox
-            checked={
-              table.getIsAllPageRowsSelected() ||
-              (table.getIsSomePageRowsSelected() && 'indeterminate')
-            }
-            onCheckedChange={(value) =>
-              table.toggleAllPageRowsSelected(!!value)
-            }
-            aria-label="Select all"
-          />
-        ),
-        cell: ({ row }) => (
-          <Checkbox
-            checked={row.getIsSelected()}
-            onCheckedChange={(value) => row.toggleSelected(!!value)}
-            aria-label="Select row"
-          />
-        ),
-        size: 40,
-        maxSize: 40,
-        enableColumnFilter: false,
-      },
-      {
         accessorKey: 'name',
         cell: (info) => info.getValue(),
-        size: 800,
-        maxSize: 800,
         meta: {
-          filterVariant: 'select',
+          filterVariant: 'text',
         },
       },
       {
-        accessorKey: 'contactName',
-        cell: (info) => info.getValue(),
-        size: 800,
-        maxSize: 800,
-        meta: {
-          filterVariant: 'select',
+        accessorKey: 'address',
+        id: 'address',
+        cell: (info) => {
+          const addr = info.getValue() as TStore['address'];
+          return (
+            <address className="whitespace-normal">
+              {addr.country}, {addr.state}, {addr.city}, {addr.zone},{' '}
+              {addr.landmark}
+            </address>
+          );
         },
+        header: () => <span>Address</span>,
+        size: 350,
+        maxSize: 350,
       },
     ],
     [],
   );
 
   const { state, handlers } = useGridState();
-
-  console.log(state);
 
   return (
     <section className="mun:py-10">
