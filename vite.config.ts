1ceba50c8861d1bbdf7e7e07d4bcc3e6763e03b5
jsdev-robin <jsdev.robin@gmail.com>
2026-07-09 22:10:53 +0600

    feat: add rollup-plugin-copy for copying README.md to dist during build

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,30 @@ import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import copyPkg, { type CopyOptions } from 'rollup-plugin-copy';
+import { defineConfig, type Plugin } from 'vite';
 import dts from 'vite-plugin-dts';
+
+const copy = copyPkg as unknown as (options: CopyOptions) => Plugin;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
+    tailwindcss({
+      optimize: true,
+    }),
     dts({
       tsconfigPath: './tsconfig.app.json',
     }),
+    {
+      ...copy({
+        targets: [{ src: 'README.md', dest: 'dist' }],
+        hook: 'writeBundle',
+      }),
+      apply: 'build',
+    },
   ],
   resolve: {
     alias: {
