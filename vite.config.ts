a2b717691f45396430e9114ee64fab17fcad388f
jsdev-robin <jsdev.robin@gmail.com>
2026-07-09 22:07:42 +0600

    feat: update package.json and pnpm-lock.yaml for improved module exports and add vite-plugin-dts for TypeScript support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,10 +11,30 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+    }),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/package/index.tsx'),
+      name: 'munza-x-data-grid',
+      fileName: 'data-grid',
+      formats: ['es', 'cjs'],
+    },
+    rolldownOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
 });
