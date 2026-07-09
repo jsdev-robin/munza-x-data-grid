b0f98aac2ad74d0b62a1cc1be21888d17eea4d07
jsdev-robin <jsdev.robin@gmail.com>
2026-07-09 20:08:37 +0600

    feat: implement vehicle management table with dummy data and update configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,145 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  type ColumnDef,
+} from '@tanstack/react-table';
+import { useMemo } from 'react';
+import { dummyVehicles, type Vehicle } from './data/dummyVehicles';
+
 const App = () => {
-  return <div className="bg-red-800">App</div>;
+  'use no memo';
+  const columns = useMemo<ColumnDef<Vehicle, unknown>[]>(
+    () => [
+      {
+        id: 'driver',
+        accessorKey: 'driver',
+        cell: (info) => info.getValue(),
+        header: () => <div>Driver</div>,
+        enableColumnFilter: true,
+      },
+      {
+        id: 'vehicle',
+        accessorKey: 'vehicle',
+        cell: (info) => info.getValue(),
+        header: () => <div>Vehicle</div>,
+      },
+      {
+        id: 'vin',
+        accessorKey: 'vin',
+        cell: (info) => info.getValue(),
+        header: () => <div>VIN</div>,
+      },
+      {
+        id: 'type',
+        accessorKey: 'type',
+        cell: (info) => info.getValue(),
+        header: () => <div>Type</div>,
+      },
+      {
+        id: 'status',
+        accessorKey: 'status',
+        cell: (info) => info.getValue(),
+        header: () => <div>Status</div>,
+      },
+      {
+        id: 'currentMeter',
+        accessorKey: 'currentMeter',
+        cell: (info) => info.getValue(),
+        header: () => <div>Current Meter</div>,
+      },
+      {
+        id: 'fuel',
+        accessorKey: 'fuel',
+        cell: (info) => info.getValue(),
+        header: () => <div>Fuel</div>,
+      },
+      {
+        id: 'drivingTime',
+        accessorKey: 'drivingTime',
+        cell: (info) => info.getValue(),
+        header: () => <div>Driving Time</div>,
+      },
+      {
+        id: 'license',
+        accessorKey: 'license',
+        cell: (info) => info.getValue(),
+        header: () => <div>License</div>,
+      },
+      {
+        id: 'odometer',
+        accessorKey: 'odometer',
+        cell: (info) => info.getValue(),
+        header: () => <div>Odometer</div>,
+      },
+    ],
+    [],
+  );
+
+  const table = useReactTable({
+    data: dummyVehicles,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <section>
+      <div className="container py-10">
+        <Table>
+          <TableHeader className="sticky top-0 z-10 bg-muted">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    style={{
+                      width: header.getSize(),
+                      minWidth: header.getSize(),
+                      maxWidth: header.getSize(),
+                    }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id} data-state={row.getIsSelected()}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell
+                    key={cell.id}
+                    style={{
+                      width: cell.column.getSize(),
+                      minWidth: cell.column.getSize(),
+                      maxWidth: cell.column.getSize(),
+                    }}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </section>
+  );
 };
 
 export default App;
