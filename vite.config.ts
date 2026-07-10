14d8128e455f4b75f7ddf0f852f4144481008e97
jsdev-robin <jsdev.robin@gmail.com>
2026-07-10 11:34:46 +0600

    feat: integrate Tailwind CSS and update Vite configuration
    - Removed existing CSS variables and styles from index.css, replacing them with Tailwind CSS import.
    - Updated vite.config.ts to include Tailwind CSS plugin and added TypeScript declaration file support with vite-plugin-dts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from '@rolldown/plugin-babel';
+import tailwindcss from '@tailwindcss/vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+    }),
+    tailwindcss(),
   ],
-})
+});
